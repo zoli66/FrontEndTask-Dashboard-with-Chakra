9d192265a4848424527d98550be92794c233c61b
Zolfaghar_A <abbasi.z1366@yahoo.com>
2026-02-10 21:35:56 +0330

    feat(layout): add simple header with user info and logout

--- src/layout/DashboardLayout.tsx
+++ src/layout/DashboardLayout.tsx
@@ -1,5 +1,7 @@
 import { Box, Flex } from "@chakra-ui/react";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
 
 function DashboardLayout() {
   return (
---
+++ src/layout/Header.tsx
@@ -0,0 +1,42 @@
+import { Avatar, Button, Flex, HStack, Text } from "@chakra-ui/react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { logout } from "../store/slices/authSlice";
+
+function Header() {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.auth.user);
+
+  return (
+    <Flex
+      bg="white"
+      h="60px"
+      align="center"
+      justify="space-between"
+      px={6}
+      boxShadow="sm"
+      position="sticky"
+      top={0}
+      zIndex={10}
+    >
+      {/*عنوان صفحه*/}
+      <Text fontWeight="bold" fontSize="lg">
+        Dashboard
+      </Text>
+
+      {/*بخش کاربر*/}
+      <HStack gap={4}>
+        {user && <Text>{user.username}</Text>}
+        {user && (
+          <Avatar.Root shape="square" size="lg">
+            <Avatar.Fallback name={`${user.firstName} ${user.lastName}`} />
+            <Avatar.Image src={user.image} />
+          </Avatar.Root>
+        )}
+        <Button colorScheme="red" size="sm" onClick={() => dispatch(logout())}>
+          Logout
+        </Button>
+      </HStack>
+    </Flex>
+  );
+}
+export default Header;
